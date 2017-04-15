67e617cf0723e0ab3074c37091c68467b13dde40
Paul Pistorius <jpistoriusp@gmail.com>
2017-04-15 16:10:33 -0600

    Working add jet option

--- src/jets/FleetMenu.java
+++ src/jets/FleetMenu.java
@@ -4,15 +4,18 @@ import java.util.*;
 
 public class FleetMenu {
 	public static void main(String[] args) {
-		start();
+		FleetMenu fm = new FleetMenu();
+		fm.start();
 	}
 
-	public static void start() {
-		Jet jetArray [] = initialLoad();
+	public void start() {
+	
 		Scanner kb = new Scanner(System.in);
 		Hangar[] PaulsHangar = new Hangar[5];
-        int choice=0;
-		
+		int choice = 0;
+		Hangar showhang = new Hangar();
+		Jet jetArray[] = showhang.initialLoad();
+
 		while (choice != 5) {
 			System.out.println("Welcome to your fleet menu. Please select an option below: ");
 			System.out.println("1: List fleet");
@@ -23,7 +26,6 @@ public class FleetMenu {
 			choice = kb.nextInt();
 			switch (choice) {
 			case 1:
-				Hangar showhang = new Hangar();
 				showhang.displayHangar(jetArray);
 				break;
 			case 2:
@@ -31,34 +33,15 @@ public class FleetMenu {
 			case 3:
 				break;
 			case 4:
-				System.out.println("Enter jet model: ");
-				String model = kb.next();
-				System.out.println("Enter jet speed (mph): ");
-				double mphSpeed = kb.nextDouble();
-				MachCalc mm = new MachCalc();
-				double speed = mm.mphToMach(mphSpeed);
-				System.out.println("Enter jet range: ");
-				int range = kb.nextInt();
-				System.out.println("Enter jet price: ");
-				int price = kb.nextInt();
-				Hangar addhang = new Hangar();
-				addhang.newJet(model, speed, range, price);
-				
+				jetArray = showhang.newJet(jetArray);
+				showhang.displayHangar(jetArray);
 				break;
 			}
 		}
 
 	}
-	public static Jet[] initialLoad(){
-		Jet jet [] = new Jet[5];
-		jet[0] = new Jet("Big Billy", 1.05, 1500, 700);
-		jet[1] = new Jet("Small Sammy", .60, 900, 50);
-		jet[2] = new Jet("Midsize Mary", .86, 1300, 300);
-		jet[3] = new Jet("Large Larry", .98, 1400, 400);
-		jet[4] = new Jet("Petite Pauly", .56, 800, 25);
-		return jet;
-	}
-	public void addJet () {
-		
+
+	public void addJet() {
+
 	}
 }
--- src/jets/Hangar.java
+++ src/jets/Hangar.java
@@ -1,8 +1,19 @@
 package jets;
 
-public class Hangar {
-	Jet[] jet = new Jet[5];
+import java.util.Scanner;
 
+public class Hangar {
+	Jet[] jet = new Jet[100];
+
+	public Jet[] initialLoad(){
+		jet[0] = new Jet("Big Billy", 1.05, 1500, 700);
+		jet[1] = new Jet("Small Sammy", .60, 900, 50);
+		jet[2] = new Jet("Midsize Mary", .86, 1300, 300);
+		jet[3] = new Jet("Large Larry", .98, 1400, 400);
+		jet[4] = new Jet("Petite Pauly", .56, 800, 25);
+		return jet;
+	}
+	
 	public void displayHangar(Jet[] jets) {
 		for (int i = 0; i < jets.length; i++) {
 			if (jets[i] != null) {
@@ -11,13 +22,27 @@ public class Hangar {
 		}
 	}
 
-	public void newJet(String model, double speed, int range, int price) {
-		Jet[] temp = new Jet[jet.length + 1];
-		for (int i = 0; i < jet.length; i++) {
-			temp[i] = jet[i];
+	public Jet[]  newJet(Jet[] jets) {
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Enter jet model: ");
+      	String model = kb.next();
+		System.out.println("Enter jet speed (mph): ");
+		double mphSpeed = kb.nextDouble();
+		MachCalc mm = new MachCalc();
+		double speed = mm.mphToMach(mphSpeed);
+		System.out.println("Enter jet range: ");
+		int range = kb.nextInt();
+		System.out.println("Enter jet price: ");
+		int price = kb.nextInt();
+		Jet jet = new Jet(model, speed, range,price);
+		for (int i = 0; i < jets.length; i++) {
+			if (jets[i] == null){
+				jets[i] = jet;
+				break;
+			}
 		}
-		temp[jet.length] = new Jet(model, speed, range, price);
-		jet = temp;
+		return jets;
 	}
+
 
 }
